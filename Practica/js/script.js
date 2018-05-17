1aef99612deda2e3879c96731c2b2c6c3d87c099
Merci13 <mrjorxe@gmail.com>
2018-05-17 11:00:41 -0600

    actulizacion
    se termino el worshop

--- Practica/js/script.js
+++ Practica/js/script.js
@@ -1,5 +1,60 @@
-function realizarAccion() {
-	var accion = document.getElementById("#accion").value;
+$("#realizarAccion").click(function() {
+    var accion = $("#accion").val();
+    var numeroA = $("#numeroA").val();
+    var numeroB = $("#numeroB").val();
+    var resultado = null;
 
-	alert(accion);
-}
+    alert(accion);
+
+    switch (accion) {
+        case "por":
+            if (numeroA == "" || numeroB == "") {
+                resultado = "";
+                break
+
+            } else {
+                resultado = numeroA * numeroB;
+
+                break;
+            }
+        case "mas":
+            if (numeroA == "" || numeroB == "") {
+                resultado = "";
+                break;
+            } else {
+                resultado = parseInt(numeroA) + parseInt(numeroB);
+                break;
+            }
+
+        case "menos":
+            if (numeroA == "" || numeroB == "") {
+                resultado = "";
+                break;
+            } else {
+                resultado = numeroA - numeroB;
+                break;
+            }
+
+
+        case "dividir":
+            if (numeroA == "" || numeroB == "") {
+                resultado = "";
+                break;
+            } else {
+
+
+                if (numeroB == 0) {
+                    alert("no se puede dividir entre 0");
+                    resultado="";
+                    break;
+
+                } else {
+                    resultado = numeroA / numeroB;
+                    break;
+                }
+            }
+    };
+
+    $("#resultado").val(resultado);
+
+});
